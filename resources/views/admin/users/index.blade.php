b200974db0829f28e20ac9aee377b269ea721086
Fabio Silva <fabinhoevil16@hotmail.com>
2025-02-19 11:16:04 -0300

    create list users

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -5,11 +5,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Usuarios</title>
 </head>
 
 <body>
-    <h1>Olá {{ $user->name }}</h1>
+    <h1>Usuários</h1>
+    <table>
+        <thead>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Ações</th>            
+        </thead>
+        <tbody>
+            @forelse ($users as $user)
+                <tr>
+                    <td> {{ $user->name }} </td>
+                    <td> {{ $user->email }} </td>
+                    <td>-</td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="100">Nenhum usuário no banco</td>
+                </tr>    
+            @endforelse
+        </tbody>
+    </table>
+
+    {{ $users->links() }}
+
 </body>
 
 </html>
